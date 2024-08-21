66b5eed883be437f6a41e980372bb2bb2ff80276
23348918 <23348918@student.uwa.edu.au>
2024-08-21 12:01:47 +0800

    LRS-23 LRS-24 added brightness, darkness and gaussian blur

--- filters.py
+++ filters.py
@@ -1,21 +1,20 @@
-from PIL import Image
+from PIL import Image, ImageEnhance, ImageFilter
 
 def darkness_filter(image: Image.Image, strength: float) -> Image.Image:
-    print("darkness")
-    return image
+    enhancer = ImageEnhance.Brightness(image)
+    return enhancer.enhance(1.0 - strength)
 
 def brightness_filter(image: Image.Image, strength: float) -> Image.Image:
-    print("darkness")
-    return image
+    enhancer = ImageEnhance.Brightness(image)
+    return enhancer.enhance(strength*2 + 1.0)
 
 def gaussian_blur_filter(image: Image.Image, strength: float) -> Image.Image:
-    print("darkness")
-    return image
+    return image.filter(ImageFilter.GaussianBlur(radius=strength*5))
 
 def colour_shift_filter(image: Image.Image, strength: float) -> Image.Image:
-    print("darkness")
+    print("colour shift")
     return image
 
 def motion_blur_filter(image: Image.Image, strength: float) -> Image.Image:
-    print("darkness")
+    print("motion blur")
     return image
