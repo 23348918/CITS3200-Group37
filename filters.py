da08cccb92b83c324905b78500ddaf1645ba243a
23348918 <23348918@student.uwa.edu.au>
2024-08-21 11:48:41 +0800

    LRS19 Construct program for calling filters.

---
+++ filters.py
@@ -0,0 +1,21 @@
+from PIL import Image
+
+def darkness_filter(image: Image.Image, strength: float) -> Image.Image:
+    print("darkness")
+    return image
+
+def brightness_filter(image: Image.Image, strength: float) -> Image.Image:
+    print("darkness")
+    return image
+
+def gaussian_blur_filter(image: Image.Image, strength: float) -> Image.Image:
+    print("darkness")
+    return image
+
+def colour_shift_filter(image: Image.Image, strength: float) -> Image.Image:
+    print("darkness")
+    return image
+
+def motion_blur_filter(image: Image.Image, strength: float) -> Image.Image:
+    print("darkness")
+    return image
